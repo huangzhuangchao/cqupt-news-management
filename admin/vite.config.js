58ca8d48f3d1e1d2fd56026cd7935a5b806045bf
huangzhuangchao <2393026981@qq.com>
2024-11-21 14:49:26 +0800

    划分server文件结构

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -51,12 +51,17 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      '/users': {
+      '/adminapi': {
         target: 'http://localhost:3000',
         changeOrigin: true,
         secure: false, // 如果是https接口，需要配置这个参数
-        rewrite: (path) => path.replace(/^\/users/, '')
+        rewrite: (path) => path.replace(/^\/adminapi/, '')
       }
     }
   },
 })
+
+/* 
+  adminapi/* 后台系统用的
+  webapi/* 企业官网用的
+*/
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* 这个文件只用于注册子路由，具体处理逻辑在controllers文件夹. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
